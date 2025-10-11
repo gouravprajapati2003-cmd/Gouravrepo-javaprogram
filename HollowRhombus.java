e2880ad2f958696220bba7bc25592ab6d49d507b
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2025-10-11 21:18:32 +0530

    Star Design

---
+++ HollowRhombus.java
@@ -0,0 +1,26 @@
+public class HollowRhombus {
+    public static void main(String[] args) {
+        int n = 5; 
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1 || i == n) {
+            
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                System.out.print("*");
+                for (int j = 1; j <= n - 2; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
