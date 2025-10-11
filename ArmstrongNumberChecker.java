040594d284121d13b74a1f2bf0fdc7e81dffb1ce
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2025-10-11 16:51:15 +0530

    my first file

---
+++ ArmstrongNumberChecker.java
@@ -0,0 +1,25 @@
+public class ArmstrongNumberChecker {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 12345;
+        int len = String.valueOf(num).length();
+      while (num != 0) {
+        int base = num % 10;
+        int exp = len;
+        int power =1;
+        for(int i=0; i<exp; i++){
+            power = power * base;
+        }
+        sum = sum + power;
+        num = num / 10;
+    }
+    if ( sum == num) {
+        System.out.println("The number is Armstrong");
+    } else {
+        System.out.println("The number is not Armstrong");
+    }
+   
+    }
+}
+   
+
